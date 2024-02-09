38964c959d49811fdc08d02b1453e430dc487f5b
Muhammad Zahalqa <m@tryfinally.com>
2024-02-09 15:46:21 +0200

    Add db write test

---
+++ satellite-subsystems/demo/src/tests/database_tests.c
@@ -0,0 +1,52 @@
+/*
+ * database_tests.c
+ *
+ *  Created on: Feb 9, 2024
+ *      Author: tryfinally
+ */
+
+#include "database_tests.h"
+#include "modules/m_db.h"
+
+#include "utils/input.h"
+#include "utils/timeutils.h"
+#include "utils/menu_selection.h"
+
+#include "utils/ulog_sqlite.h"
+#include <hal/Timing/Time.h>
+#include <at91/utility/rand.h>
+#include <hcc/api_fat.h>
+#include <stdio.h>
+
+
+
+
+static Boolean db_write_simple_test(void)
+{
+	m_db_init();
+	if (db_write_data(TELEMETRY_RADFET_PAYLOAD, 9, 0)) {
+		printf("Pass\n");
+	} else {
+		printf("Fail\n");
+	}
+
+	return TRUE;
+}
+
+
+static MenuAction menu[] = {
+		{ db_write_simple_test, "DB write simple" },
+
+		MENU_ITEM_END
+};
+
+
+Boolean database_tests(void)
+{
+	// Thursday, February 1, 2024 12:00:00 PM  GMT
+	const unsigned int epoch_time = 1706788800;
+	Time_setUnixEpoch(epoch_time);
+
+	MenuDisplay(menu);
+	return TRUE;
+}
---
+++ satellite-subsystems/demo/src/tests/database_tests.h
@@ -0,0 +1,16 @@
+/*
+ * database_tests.h
+ *
+ *  Created on: Feb 9, 2024
+ *      Author: tryfinally
+ */
+
+#ifndef TESTS_DATABASE_TESTS_H_
+#define TESTS_DATABASE_TESTS_H_
+
+
+#include <hal/boolean.h>
+
+Boolean database_tests(void);
+
+#endif /* TESTS_DATABASE_TESTS_H_ */
